a4332d55631fdd0376cefac131788b616edbb28f
jmarlin <j.marlin@outlook.com>
2016-02-03 18:01:55 -0500

    Another missing struct statement

--- lexer.h
+++ lexer.h
@@ -10,7 +10,7 @@ int lexer_init();
 int lexer_add_rule(char* name, char* rule);
 void lexer_print_rules(void);
 struct token_tree* lexer_run(char* input_string);
-int lexer_match(char* input_string, token_tree* tree_root);
+int lexer_match(char* input_string, struct token_tree* tree_root);
 int lexer_match_on_rule();
 
 #endif //LEXER_H
