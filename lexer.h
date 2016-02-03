1a94a696af3ea8fc26fb243ff6668a8f2b5f8abe
jmarlin <j.marlin@outlook.com>
2016-02-03 18:01:23 -0500

    Fixed token tree reference in lexer.h

--- lexer.h
+++ lexer.h
@@ -9,7 +9,7 @@ typedef struct LexerRule {
 int lexer_init();
 int lexer_add_rule(char* name, char* rule);
 void lexer_print_rules(void);
-token_tree* lexer_run(char* input_string);
+struct token_tree* lexer_run(char* input_string);
 int lexer_match(char* input_string, token_tree* tree_root);
 int lexer_match_on_rule();
 
