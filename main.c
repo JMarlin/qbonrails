c835535660c301d095d2beaab4b471f8518489e8
jmarlin <j.marlin@outlook.com>
2016-02-03 23:41:48 -0500

    More debug printing

--- main.c
+++ main.c
@@ -12,12 +12,16 @@ int main(int argc, char* argv[]) {
         return 1; 
     }
     
+    printf("one:\n");
     lexer_add_rule("literal_string", "\"(c*)\"");
-    lexer_print_rules();
+    lexer_print_rules(); 
+    printf("two:\n");
     lexer_add_rule("literal", "[literal_string]");
     lexer_print_rules();
+    printf("three:\n");
     lexer_add_rule("print_statement", "PRINT(w*)[literal](w*)");
     lexer_print_rules();
+    printf("four:\n");
     lexer_add_rule("statement", "[print_statement]");
     lexer_print_rules();
     
